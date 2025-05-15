44dc45afa323d28f319416eb2bff8edeaf936a1e
saraStevens325 <Sara_Stevens_1@proton.me>
2025-05-15 15:55:29 -0700

    added top ten bitcoins display

--- crypto-app/src/pages/Home/Home.jsx
+++ crypto-app/src/pages/Home/Home.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import './Home.css'
+import { CoinContext } from '../../context/CoinContext'
 const Home = () => {
+
+  const {allCoin, currency} = useContext(CoinContext);
+  const [displayCoin, setDisplayCoin] = useState([]);
+
+  useEffect(() => {
+    setDisplayCoin(allCoin);
+  }, [allCoin])
+
   return (
     <div className='home'>
       <div className='hero'>
-        <h1>Largets <br/> Cyrpto Marketplace</h1>
-        <p>Welcome to the worlds largest cryptocurrency marketplace. Sign up to explore more about cryptos.</p>
+        <h1>Crypto Marketplace</h1>
+        <p>Welcome to my cryptocurrency marketplace. Sign up to explore and track your favorite cryptocurrencies.</p>
         <form>
           <input type="text" placeholder='Search Crypto' />
           <button type='submit'>Search</button>
@@ -19,6 +28,28 @@ const Home = () => {
           <p style={{textAlign: "center"}}>24h Change</p>
           <p className='market-cap'>Market Cap</p>
           </div>
+          {
+            displayCoin.slice(0, 10).map((item, index) => (
+              <div className="table-layout" key={index}>
+                <p>
+                  {item.market_cap_rank}
+                </p>
+                <div>
+                  <img src={item.image} alt="" />
+                  <p>{item.name + " - "+ item.symbol}</p>
+                  
+                  
+                </div>
+                <p>{currency.symbol} {item.current_price.toLocaleString()}</p>
+                <p className={item.price_change_percentage_24h>0? "green" : "red"}>{Math.floor(item.price_change_percentage_24h* 100)/100 }</p>
+                <p className="market-cap">{currency.symbol} {item.market_cap.toLocaleString()}</p>
+
+              
+                </div>
+            ))
+          }
+
+          
           </div>
 
       
