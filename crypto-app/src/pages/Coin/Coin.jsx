e82d7606981f96195c0df0343888c18964620d43
saraStevens325 <Sara_Stevens_1@proton.me>
2025-05-21 13:01:35 -0700

    api endpoint for coin data and population of line chart complete

--- crypto-app/src/pages/Coin/Coin.jsx
+++ crypto-app/src/pages/Coin/Coin.jsx
@@ -2,10 +2,12 @@ import React, { useEffect, useContext, useState } from 'react'
 import './Coin.css'
 import { useParams } from 'react-router-dom'
 import { CoinContext } from '../../context/CoinContext'
+import LineChart from '../../components/LineChart/LineChart'
 
 const Coin = () => {
   const { coinId } = useParams()
   const [coinData, setCoinData] = useState();
+  const [historicalData, setHistoricalData] = useState();
   const {currency} = useContext(CoinContext);
 
   const fetchCoinData = async () => {
@@ -23,21 +25,42 @@ const Coin = () => {
             .catch(err => console.error(err));
   }
 
+  const fetchHistoricalData = async () => {
+    
+  const options = {
+            method: 'GET',
+            headers: {
+              accept: 'application/json',
+              // 'x-cg-demo-api-key': '	CG-eEHuo7jMnXvTLVF2A5rX5zhh'
+            }}
+          
+
+    fetch(`https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=${currency.name}&days=10&interval=daily`, options)
+      .then(res => res.json())
+      .then(res => setHistoricalData(res))
+      .catch(err => console.error(err));
+  }
+    
+
   useEffect(() => {
     fetchCoinData();
-  }, [currency])
+    fetchHistoricalData();
+  }, [currency, coinId])
 
-if(coinData){
-  return (
-    <div className='coin'>
-      <div className="coinName">
-        <img src={coinData.image.large} alt='' />
-        <p><b>{coinData.name} ({coinData.symbol.toUpperCase()})</b></p>
-      </div>
+if(coinData && historicalData) {
+      return (
+        <div className='coin'>
+          <div className="coinName">
+            <img src={coinData.image.large} alt='' />
+            <p><b>{coinData.name} ({coinData.symbol.toUpperCase()})</b></p>
+          </div>
+          <div className="coin-chart">
+            <LineChart historicalData={historicalData}/>
+          </div>
 
-      
-    </div>
-  )
+          
+        </div>
+      )
 } else {
   return (
     <div className='spinner'>
@@ -45,7 +68,8 @@ if(coinData){
     </div>
   )
 }
-  
 }
+  
+
 
 export default Coin
